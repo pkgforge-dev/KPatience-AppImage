5a1cbac6f2c98edb333a1ce0ae3867fdece145d0
Samuel <36420837+Samueru-sama@users.noreply.github.com>
2026-04-18 22:57:08 -0400

    add `kiconthemes-mini`

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -14,7 +14,7 @@ pacman -Syu --noconfirm \
 
 echo "Installing debloated packages..."
 echo "---------------------------------------------------------------"
-get-debloated-pkgs --add-common --prefer-nano
+get-debloated-pkgs --add-common --prefer-nano kiconthemes-mini
 
 # Comment this out if you need an AUR package
 #make-aur-package PACKAGENAME
