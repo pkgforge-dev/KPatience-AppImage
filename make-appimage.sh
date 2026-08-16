11c2822b182185d5f3f96601fb7bd30ba7621f11
Link4Electronics <link4electronics@gmail.com>
2026-08-15 21:03:50 -0300

    Update make-appimage.sh

--- make-appimage.sh
+++ make-appimage.sh
@@ -3,7 +3,7 @@
 set -eu
 
 ARCH=$(uname -m)
-VERSION=$(pacman -Q kpat | awk '{print $2; exit}') # example command to get version of application here
+VERSION=$(pacman -Q kpat | awk '{print $2; exit}')
 export ARCH VERSION
 export OUTPATH=./dist
 export ADD_HOOKS="self-updater.hook"
@@ -15,8 +15,6 @@ export DEPLOY_PULSE=1
 
 # Deploy dependencies
 quick-sharun /usr/bin/kpat /usr/share/kpat /usr/share/knsrcfiles /usr/share/config.kcfg /usr/lib/libKirigami*.so* /usr/share/carddecks
-
-# Additional changes can be done in between here
 echo 'ANYLINUX_DO_NOT_LOAD_LIBS=libpipewire-0.3.so*:${ANYLINUX_DO_NOT_LOAD_LIBS}' >> ./AppDir/.env
 
 # Turn AppDir into AppImage
